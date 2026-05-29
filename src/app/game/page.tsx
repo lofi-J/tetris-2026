9161d2de71378642a59f9438226d9c96558e8fe5
lofi <jsj2505@gmail.com>
2026-05-29 13:47:04 +0900

    feat: add route /game

---
+++ src/app/game/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Game() {
+  return (
+    <div className="flex flex-col justify-center items-center flex-1">
+      <h1>PG</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">Hello World</h1>
+      <Link href="/game">Press Any Key to Start</Link>
     </div>
   );
 }
